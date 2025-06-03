9bce9d4d28cea4c2b8a6fbbac0a6aaa621382436
samjwillis97 <sam@williscloud.org>
2025-06-03 17:46:22 +1000

    removing dist directory

--- dist/cli/commands/request.d.ts
+++
@@ -1,4 +0,0 @@
-export interface RequestOptions {
-    url: string;
-}
-export declare function handleRequestCommand(options: RequestOptions): Promise<void>;
--- dist/index.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env node
-import './cli/main.js';
-//# sourceMappingURL=index.js.map
